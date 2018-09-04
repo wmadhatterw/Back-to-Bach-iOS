97fa572b15abca3ae43c56cf328d9f22ff9efe28
Seth Walton <sdwalton83@gmail.com>
2018-09-04 06:19:08 -0500

    finished

--- Back to Bach/ViewController.swift
+++ Back to Bach/ViewController.swift
@@ -7,12 +7,66 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
-
+    @IBOutlet var scrubber: UISlider!
+    var player = AVAudioPlayer()
+    let audioPath = Bundle.main.path(forResource: "bach", ofType: "mp3")
+    var timer = Timer()
+    @objc func updateScrubber(){
+        scrubber.value = Float(player.currentTime)
+    }
+    @IBAction func play(_ sender: Any) {
+        player.play()
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateScrubber), userInfo: nil, repeats: true)
+    }
+    
+    
+    @IBAction func pause(_ sender: Any) {
+        player.pause()
+        timer.invalidate()
+    }
+    
+    @IBAction func Stop(_ sender: Any) {
+        player.pause()
+        timer.invalidate()
+        
+        do {
+            
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+           
+        }catch {
+            //process error
+        }
+    }
+    
+    @IBAction func volumeChanged(_ sender: Any) {
+        player.volume = volumeSlider.value
+    }
+    
+    @IBOutlet var volumeSlider: UISlider!
+    
+    @IBAction func scrubberMoved(_ sender: Any) {
+        player.currentTime = TimeInterval(scrubber.value)
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        do {
+            
+            try player = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
+            scrubber.maximumValue = Float(player.duration)
+            
+            
+        }catch {
+            //process error
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
